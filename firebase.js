e95d10f1e13998c58fe1aa1bc806c07e5c1ee1cd
Benedict G. Rosas <benedictrosas2@gmail.com>
2025-07-07 19:50:40 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,4 @@
-// firebase.js - Initializes Firebase and exports services
-
-// Import the functions you need from the SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const App = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,22 +16,6 @@ const firebaseConfig = {
   measurementId: "G-NMXS5E7YD8"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-const appId = typeof __app_id !== 'undefined' ? __app_id : firebaseConfig.appId;
-const initialAuthToken = typeof __initial_auth_token !== 'undefined' ? __initial_auth_token : null;
-
-
-export {
-    auth,
-    db,
-    appId, // Export appId for use in Firestore paths
-    initialAuthToken, // Export initialAuthToken for Canvas environment
-    // Export specific auth functions needed in main.js
-    signInAnonymously,
-    signInWithCustomToken,
-    onAuthStateChanged
-};
+const analytics = getAnalytics(app);
